88061c5c4dc08ff3075c099621725024cc5d0d55
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-03-11 19:31:58 -0600

    Declaracion de implementaciones Map

---
+++ src/TreeMap.java
@@ -0,0 +1,75 @@
+/**
+ * @author Walter Saldaña #19897
+ * @author Marco Ramirez # 
+ * Implementacion de tipo Tree de un Map
+ */
+
+import java.util.Set;
+
+public class TreeMap<K,V> implements Map<K,V>{
+    
+    public TreeMap(){
+
+    }
+
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+    public boolean containsKey(K k) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+    public boolean containsValue(V v) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+    public V get(K k) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    public V put(K k, V v) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    public V remove(K k) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    public void putAll(Map<K, V> other) {
+        // TODO Auto-generated method stub
+
+    }
+    
+    public void clear() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public Set<K> keySet() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Structure<V> values() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Set<Association<K, V>> entrySet() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
